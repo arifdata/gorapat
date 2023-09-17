6f9831055bd7a19eed23cf7a9cffbd1bf2b58c1c
Arif Mulpratama <arifdata7@gmail.com>
2023-09-17 19:47:51 +0700

    berhasil implementasi events as function pakai fetch

--- frontend/index.js
+++ frontend/index.js
@@ -4,37 +4,14 @@ import dayGridPlugin from '@fullcalendar/daygrid'
 import timeGridPlugin from '@fullcalendar/timegrid'
 import listPlugin from '@fullcalendar/list'
 import './index.css'
-import bootstrap5Plugin from '@fullcalendar/bootstrap5';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap-icons/font/bootstrap-icons.css';
-import translateID from '@fullcalendar/core/locales/id';
-import PocketBase from 'pocketbase';
-
-/*const pb = new PocketBase('http://127.0.0.1:8090');
-const resultList = await pb.collection('event').getList(1, 50, {
-  filter: 'created >= "2023-01-01 00:00:00"',
-});
-console.log(resultList.items[0]['created']);*/
+import bootstrap5Plugin from '@fullcalendar/bootstrap5'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'
+import translateID from '@fullcalendar/core/locales/id'
 
 document.addEventListener('DOMContentLoaded', async function () {
-  var list_events = [];
-  const calendarEl = document.getElementById('calendar');
-  const pb = new PocketBase('http://127.0.0.1:8090');
-  const resultList = await pb.collection('event').getList(1, 50, {
-    filter: 'start >= "2023-09-01 00:00:00"',
-    expand: 'ruang',
-  });
-  const ev = resultList.items;
-  console.log(ev);
 
-  for (var i = 0; i < ev.length; i++) {
-    list_events.push({
-      title: ev[i]['title'],
-      start: ev[i]['start'],
-      end: ev[i]['end'],
-      backgroundColor: ev[i]['expand']['ruang']['label_warna'],
-    });
-  };
+  const calendarEl = document.getElementById('calendar');
 
   const calendar = new Calendar(calendarEl, {
     timeZone: 'UTC',
@@ -55,65 +32,30 @@ document.addEventListener('DOMContentLoaded', async function () {
     navLinks: true, // can click day/week names to navigate views
     editable: false,
     dayMaxEvents: true, // allow "more" link when too many events
-    /*events: [
-      {
-        title: 'All Day Event',
-        start: '2023-09-01',
-      },
-      {
-        title: 'Long Event',
-        start: '2023-09-07',
-        end: '2023-09-10',
-        color: '#ff0000',
-      },
-      {
-        groupId: 999,
-        title: 'Repeating Event',
-        start: '2023-09-09T16:00:00',
-      },
-      {
-        groupId: 999,
-        title: 'Repeating Event',
-        start: '2023-09-16T16:00:00',
-      },
-      {
-        title: 'Conference',
-        start: '2023-09-11',
-        end: '2023-09-13',
-      },
-      {
-        title: 'Meeting',
-        start: '2023-09-12T10:30:00',
-        end: '2023-09-12T12:30:00',
-      },
-      {
-        title: 'Lunch',
-        start: '2023-09-12T12:00:00',
-      },
-      {
-        title: 'Meeting',
-        start: '2023-09-12T14:30:00',
-      },
-      {
-        title: 'Happy Hour',
-        start: '2023-09-12T17:30:00',
-      },
-      {
-        title: 'Dinner',
-        start: '2023-09-12T20:00:00',
-      },
-      {
-        title: 'Birthday Party',
-        start: '2023-09-13T07:00:00',
-
-      },
-      {
-        title: 'Click for Google',
-        url: 'http://google.com/',
-        start: '2023-09-28',
-      },
-    ],*/
-    events: list_events,
+    
+    events: function (info, successCallback, failureCallback){
+      const rentangAwal = info.start.toISOString().slice(0,10);
+      const rentangAkhir = info.end.toISOString().slice(0,10);
+      fetch(`http://localhost:8090/api/collections/event/records?page=1&perPage=500&filter=start%20%3E%3D%20%22${rentangAwal}%22%20%26%26%20end%20%3C%3D%20%22${rentangAkhir}%22&expand=ruang`)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        let daftar_event = [];
+        for (var i=0; i < data.items.length; i++){
+          daftar_event.push({
+            title: data.items[i]['title'],
+            start: data.items[i]['start'],
+            end: data.items[i]['end'],
+            backgroundColor: data.items[i]['expand']['ruang']['label_warna'],
+          });
+        };
+        successCallback(daftar_event);
+      })
+      .catch((err) => {
+        failureCallback(err);
+      });
+    },
     eventTimeFormat: {
       hour: '2-digit',
       minute: '2-digit',
